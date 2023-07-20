311c24b4de65996032e0778ac13358e887294218
LNR14 <lnrabc@gmail.com>
2023-07-20 18:44:15 -0500

    Section 5 done

---
+++ learn-spring-boot/learn-spring-boot/src/main/java/com/springcourse/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,26 @@
+package com.springcourse.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//   /courses
+//return course details
+
+
+@RestController
+public class CourseController {
+
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				
+				new Course(1,"AWS","in28minutes"),
+				new Course(2,"Learn DevOps","in28minutes"),
+				new Course(3,"Learn Azure","in28minutes")
+				);
+	}
+	
+}
